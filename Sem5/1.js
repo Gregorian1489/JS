514efde670b261a02964cda6e5b8ad4d33307b19
Gregorian1489 <santijago@mail.ru>
2024-01-23 15:41:31 +0400

    add HW05

---
+++ Sem5/1.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+Дан объект numbers. 
+Необходимо в консоль вывести все значения объекта, которые больше или равны 3.
+*/
+
+const numbers = {
+  key1: 12,
+  key2: 2,
+  key3: 4,
+  key4: 1,
+  key5: -244,
+  key6: 0,
+  key7: 7,
+};
+
+console.log('Task 1');
+console.log(numbers);
+console.log("Значения больше или равны 3: " + Object.values(numbers).filter((value) => value >= 3));
